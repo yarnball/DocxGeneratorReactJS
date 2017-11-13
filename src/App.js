5ee14511d957272eb592984abfc192a44f426156
user1 <user1@user1s-MacBook-Pro.local>
2017-11-13 17:30:48 +1100

    Moved imports to head

--- src/App.js
+++ src/App.js
@@ -1,63 +1,53 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { render } from 'react-dom';
 import tester from './tester.docx';
-import './App.css';
 
 var JSZip = require('jszip');
 var Docxtemplater = require('docxtemplater');
 
-
-
-//Load the docx file as a binary
-
-
-class App extends Component {
+class App extends React.Component {
 
   render() {
-  function loadFile(url,callback){
-    console.log('lo', url)
-          window.JSZipUtils.getBinaryContent(url,callback);
+    function loadFile(url, callback) {
+      console.log('lo', url)
+      window.JSZipUtils.getBinaryContent(url, callback);
+    }
+    loadFile(tester, function (error, content) {
+      if (error) { throw error };
+      var zip = new JSZip(content);
+      var doc = new Docxtemplater().loadZip(zip)
+      doc.setData({
+        first_name: 'John',
+        last_name: 'Doe',
+        phone: '0652455478',
+        description: 'New Website'
+      });
+
+      try {
+        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+        doc.render()
       }
-      loadFile(tester,function(error,content){
-          if (error) { throw error };
-          var zip = new JSZip(content);
-          var doc=new Docxtemplater().loadZip(zip)
-          doc.setData({
-              first_name: 'John',
-              last_name: 'Doe',
-              phone: '0652455478',
-              description: 'New Website'
-          });
+      catch (error) {
+        var e = {
+          message: error.message,
+          name: error.name,
+          stack: error.stack,
+          properties: error.properties,
+        }
+        console.log(JSON.stringify({ error: e }));
+        // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+        throw error;
+      }
 
-          try {
-              // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-              doc.render()
-          }
-          catch (error) {
-              var e = {
-                  message: error.message,
-                  name: error.name,
-                  stack: error.stack,
-                  properties: error.properties,
-              }
-              console.log(JSON.stringify({error: e}));
-              // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
-              throw error;
-          }
-
-          var out=doc.getZip().generate({
-              type:"blob",
-              mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
-          }) //Output the document using Data-URI
-          window.saveAs(out,"output.docx")
-      })
+      var out = doc.getZip().generate({
+        type: "blob",
+        mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+      }) //Output the document using Data-URI
+      window.saveAs(out, "output.docx")
+    })
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      This is a test
       </div>
     );
   }
