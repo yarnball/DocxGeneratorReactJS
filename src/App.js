5d559e199bdd63c7e6cd5e01f7f5a08732ccc3a3
user1 <user1@user1s-MacBook-Pro.local>
2017-11-13 16:26:55 +1100

    Unable to open .docx template

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import './App.css';
 var JSZip = require('jszip');
 var Docxtemplater = require('docxtemplater');
 
-var fs = require('fs');
-var path = require('path');
+
 
 //Load the docx file as a binary
 
@@ -14,40 +13,43 @@ var path = require('path');
 class App extends Component {
 
   render() {
-    var content = fs.readFileSync(path.resolve(__dirname, 'input.docx'), 'binary');
+  function loadFile(url,callback){
+    console.log('lo', url)
+          window.JSZipUtils.getBinaryContent(url,callback);
+      }
+      loadFile("tester.docx",function(error,content){
+          if (error) { throw error };
+          var zip = new JSZip(content);
+          var doc=new Docxtemplater().loadZip(zip)
+          doc.setData({
+              first_name: 'John',
+              last_name: 'Doe',
+              phone: '0652455478',
+              description: 'New Website'
+          });
 
-    var zip = new JSZip(content);
+          try {
+              // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
+              doc.render()
+          }
+          catch (error) {
+              var e = {
+                  message: error.message,
+                  name: error.name,
+                  stack: error.stack,
+                  properties: error.properties,
+              }
+              console.log(JSON.stringify({error: e}));
+              // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
+              throw error;
+          }
 
-    var doc = new Docxtemplater();
-    doc.loadZip(zip);
-
-    doc.setData({
-        first_name: 'John',
-        last_name: 'Doe',
-        phone: '0652455478',
-        description: 'New Website'
-    });
-    try {
-        // render the document (replace all occurences of {first_name} by John, {last_name} by Doe, ...)
-        doc.render()
-    }
-    catch (error) {
-        var e = {
-            message: error.message,
-            name: error.name,
-            stack: error.stack,
-            properties: error.properties,
-        }
-        console.log(JSON.stringify({error: e}));
-        // The error thrown here contains additional information when logged with JSON.stringify (it contains a property object).
-        throw error;
-    }
-
-    var buf = doc.getZip()
-                 .generate({type: 'nodebuffer'});
-
-    // buf is a nodejs buffer, you can either write it to a file or do anything else with it.
-    fs.writeFileSync(path.resolve(__dirname, 'output.docx'), buf);
+          var out=doc.getZip().generate({
+              type:"blob",
+              mimeType: "application/vnd.openxmlformats-officedocument.wordprocessingml.document",
+          }) //Output the document using Data-URI
+          window.saveAs(out,"output.docx")
+      })
     return (
       <div className="App">
         <header className="App-header">
